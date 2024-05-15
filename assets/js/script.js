0b5e8458d4b726b8a665c2278df9fcce1cb0b2dc
Christopher Dunne <christopherdunne@outlook.ie>
2024-05-15 13:53:35 +0000

    Add base function to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Wait for the DOM to finish loading before running the game
+//Get button elements and add listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type")
+                    alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
@@ -7,7 +25,7 @@ function checkAnswer() {
 }
 
 function calculateAnswer() {
-    
+
 }
 
 function incrementScore() {
